b6aa01cf204638f26c4b2b6575935c22a78a2c6b
jairowebs <136769364+jairowebs@users.noreply.github.com>
2023-07-20 12:14:23 -0500

    .

--- app/src/main/java/www/colegiolancaster/com/MainActivity2.java
+++ app/src/main/java/www/colegiolancaster/com/MainActivity2.java
@@ -2,6 +2,7 @@
 
 package www.colegiolancaster.com;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
@@ -11,7 +12,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 
 public class MainActivity2 extends AppCompatActivity {
-    private EditText etn,etp;
+    private EditText etn, etp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,21 +20,26 @@ public class MainActivity2 extends AppCompatActivity {
         setContentView(R.layout.activity_main2);
 
 
-    etn = (EditText) findViewById (R.id.txt1) ;
-    etp = (EditText) findViewById (R.id.txt2);
+        etn = (EditText) findViewById(R.id.txt1);
+        etp = (EditText) findViewById(R.id.txt2);
     }
+
     //metodo boton
-    public  void ingresar (View View){
+    public void ingresar(View View) {
         String nombre = etn.getText().toString();
         String contrasena = etp.getText().toString();
-if (nombre.length() == 0){
-    Toast.makeText(this,"debes ingresar tu nombre",Toast.LENGTH_LONG).show();
-}
-if (contrasena.length() == 0){
-    Toast.makeText(this,"debes ingresar tu contraseña",Toast.LENGTH_LONG).show();
-}
-if (nombre.length()!= 0 && contrasena.length() != 0){
-    Toast.makeText(this,"ingresando.....",Toast.LENGTH_LONG).show();
+        if (nombre.length() == 0) {
+            Toast.makeText(this, "debes ingresar tu nombre", Toast.LENGTH_LONG).show();
+        }
+        if (contrasena.length() == 0) {
+            Toast.makeText(this, "debes ingresar tu contraseña", Toast.LENGTH_LONG).show();
+        }
+        if (nombre.length() != 0 && contrasena.length() != 0) {
+            Toast.makeText(this, "ingresando.....", Toast.LENGTH_LONG).show();
         }
     }
+
+    public void siguiente (View View){
+        Intent siguiente = new Intent(this,ProfileuserActivity.class);
+        startActivity(siguiente);}
 }
