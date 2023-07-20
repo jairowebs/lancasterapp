97190d6fce3c87d42d33e18156f3bdc8bc8a17f3
jairowebs <136769364+jairowebs@users.noreply.github.com>
2023-07-19 22:28:15 -0500

    .

---
+++ app/src/main/java/www/colegiolancaster/com/MainActivity.java
@@ -0,0 +1,28 @@
+package www.colegiolancaster.com;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+public void siguiente (View View){
+    Intent siguiente = new Intent(this,MainActivity2.class);
+    startActivity(siguiente);
+}
+
+
+
+
+
+}
