ad92c2fb09b4e45d5072428fa393d569df0459d1
Azula <patricia9312@hotmail.es>
2023-07-20 11:46:35 -0500

    creacion perfil admin

--- app/src/main/java/www/colegiolancaster/com/ProfileuserActivity.java
+++ app/src/main/java/www/colegiolancaster/com/ProfileuserActivity.java
@@ -2,7 +2,9 @@ package www.colegiolancaster.com;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class ProfileuserActivity extends AppCompatActivity {
 
@@ -11,4 +13,10 @@ public class ProfileuserActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profileuser);
     }
+
+    public void logout(View view){
+        Intent i = new Intent(this, MainActivity2.class);
+        startActivity(i);
+    }
+
 }
