f80f147e6ce83d68b7b1a7437d64084681924e12
jairowebs <136769364+jairowebs@users.noreply.github.com>
2023-07-20 21:08:18 -0500

    .

--- app/src/main/java/www/colegiolancaster/com/MainActivity2.java
+++ app/src/main/java/www/colegiolancaster/com/MainActivity2.java
@@ -5,15 +5,13 @@ package www.colegiolancaster.com;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.EditText;
-import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 
 public class MainActivity2 extends AppCompatActivity {
 
-    private EditText etn,etp;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,31 +19,16 @@ public class MainActivity2 extends AppCompatActivity {
         setContentView(R.layout.activity_main2);
 
 
-    etn = (EditText) findViewById (R.id.txt1) ;
-    etp = (EditText) findViewById (R.id.txt2);
+
 
     }
-    //metodo boton
-    public  void ingresar (View View){
-        String nombre = etn.getText().toString();
-        String contrasena = etp.getText().toString();
-if (nombre.length() == 0){
-    Toast.makeText(this,"debes ingresar tu nombre",Toast.LENGTH_LONG).show();
-}
-if (contrasena.length() == 0){
-    Toast.makeText(this,"debes ingresar tu contraseña",Toast.LENGTH_LONG).show();
-}
-if (nombre.length()!= 0 && contrasena.length() != 0){
-    Toast.makeText(this,"ingresando.....",Toast.LENGTH_LONG).show();
-        }
+    public void siguiente1 (View View){
+        Intent siguiente1 = new Intent(this,ProfileuserActivity.class);
+        startActivity(siguiente1);
     }
-      public void siguiente (View View){
-        Intent siguiente = new Intent(this,ProfileuserActivity.class);
-        startActivity(siguiente);}
-        public void siguiente1 (View View) {
-            Intent siguiente1 = new Intent(this, AdminProfileActivity.class);
-            startActivity(siguiente1);
-
+    public void siguiente2 (View View) {
+        Intent siguiente2 = new Intent(this, AdminProfileActivity.class);
+        startActivity(siguiente2);
 
         }
 }
