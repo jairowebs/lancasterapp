e9f54bb83a8b11649f255d55cb3c3127c6dd7407
Azula <patricia9312@hotmail.es>
2023-07-23 19:07:06 -0500

    firebase

---
+++ app/src/main/java/www/colegiolancaster/com/CalificacionesActivity.java
@@ -0,0 +1,14 @@
+package www.colegiolancaster.com;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CalificacionesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_calificaciones);
+    }
+}
--- app/src/main/java/www/colegiolancaster/com/ProfileuserActivity.java
+++ app/src/main/java/www/colegiolancaster/com/ProfileuserActivity.java
@@ -5,18 +5,56 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageButton;
+import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class ProfileuserActivity extends AppCompatActivity {
+
+    FirebaseAuth auth;
+    ImageButton button;
+    TextView textView;
+    FirebaseUser user;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profileuser);
+
+        auth = FirebaseAuth.getInstance();
+        button = findViewById(R.id.LogoutBtn);
+        textView = findViewById(R.id.textView2);
+        user = auth.getCurrentUser();
+        if (user == null){
+            Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
+            startActivity(intent);
+            finish();
+        } else{
+            textView.setText(user.getEmail());
+        }
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                FirebaseAuth.getInstance().signOut();
+                Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
+                startActivity(intent);
+                finish();
+
+            }
+        });
     }
 
     public void logout(View view){
         Intent i = new Intent(this, LoginActivity.class);
         startActivity(i);
+    }
+
+    public void calificaciones (View View){
+        Intent siguiente = new Intent(this,CalificacionesActivity.class);
+        startActivity(siguiente);
     }
 
 }
